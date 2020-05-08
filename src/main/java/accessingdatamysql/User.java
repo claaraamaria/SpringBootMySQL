3903e3d3e0957e9ea210b282fb8091ad6d900bab
claaraa <clrstancu@gmail.com>
2020-05-08 14:00:56 +0300

    Accessing data from MySql - REST Spring reactive

---
+++ src/main/java/accessingdatamysql/User.java
@@ -0,0 +1,41 @@
+package accessingdatamysql;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+
+    private String userName;
+
+    private String password;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
